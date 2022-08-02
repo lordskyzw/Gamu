471d37bada7f7dd5fa6642f31b98eed4897ba0f0
lordskyzw <68661064+lordskyzw@users.noreply.github.com>
2022-08-02 20:19:07 +0200

    Create gamu.py
    Taken from Modhes rib?

---
+++ gamu.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+
+
+driver = webdriver.Chrome(executable_path=r'C:\Users\im_bradley\Downloads\chromedriver_win32\chromedriver.exe')
+driver.get("https://instagram.com")
+
