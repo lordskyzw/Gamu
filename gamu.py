767d95b1b5091c0943acfe8bab0802765f924d2f
lordskyzw <68661064+lordskyzw@users.noreply.github.com>
2022-08-03 10:42:33 +0200

    fixed deprecation warning

--- gamu.py
+++ gamu.py
@@ -1,6 +1,11 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
 
-driver = webdriver.Chrome(executable_path=r'C:\Users\im_bradley\Downloads\chromedriver_win32\chromedriver.exe')
+
+
+driver = webdriver.Chrome(service=Service(r'C:\Users\im_bradley\Downloads\chromedriver_win32\chromedriver.exe'))
 driver.get("https://instagram.com")
 
+time.sleep(60)
