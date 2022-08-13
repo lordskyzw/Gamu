25c091d133700c1562bc20819f91157c4ee7f9ff
lordskyzw <68661064+lordskyzw@users.noreply.github.com>
2022-08-13 09:59:27 +0200

    Adressing user friendliness and security issue

--- gamu.py
+++ gamu.py
@@ -1,12 +1,30 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+import argparse
 import time
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from botparameters import username, password, search_item
 
+intro ='This is an Instagram bot built by Engineer Chiwara (@the.ip.boy.friend on Instagram). It goes to a targets recent post, goes through the people who liked it and follows them in hopes of them following back.'
 
-driver = webdriver.Chrome(service=Service(r'C:\Users\im_bradley\Downloads\chromedriver_win32\chromedriver.exe'))
+#defining arguments 
+parser =argparse.ArgumentParser(description=intro, usage='python gamu.py [target] [username] [password]', add_help=True)
+#adding arguments
+parser.add_argument('[target]', help='target account')
+parser.add_argument('[username]', help='username of your account')
+parser.add_argument('[password]', help='password of your account')
+#read arguments from command line
+args = parser.parse_args()
+#setting arguments into variables. RIP to RAM 
+target = args.t
+username = args.u
+password = args.p
+#just to show that arguments were taken
+print(f'Running it up fro {args.username}')
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(options=options, service=Service(r'C:\Users\im_bradley\Downloads\chromedriver_win32\chromedriver.exe'))
 
 
 
@@ -18,13 +36,12 @@ def login(username, password):
     time.sleep(30)   
     return driver
 
-def search(search_term):
+def search(target):
     
     driver.find_element(By.XPATH, '/html/body/div[1]/section/main/div/div/div/div/button').click()
-    #clearing notifiation thingy
+    #clearing notifiation thingy and searching
     driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div/div/div/div[3]/button[2]').click()
-    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input').send_keys(search_term)
-    driver.find_element(By.NAME, 'search').send_keys(Keys.RETURN)
+    driver.find_element(By.XPATH, '/html/body/div[1]/div/div/div/div[1]/div/div/div/div[1]/div[1]/section/nav/div[2]/div/div/div[2]/input').send_keys(target)
     time.sleep(10)
     return driver
     
@@ -32,4 +49,4 @@ def search(search_term):
 
 if __name__ == '__main__':
     login(username, password)
-    search(search_item)
+    search(target)
