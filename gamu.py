fcfe8621a0859e4b8e2f40a1a2fbebb44c92b235
Tarmica <lilskyforever@gmail.com>
2022-12-11 09:55:27 +0200

    Searches Target.

--- gamu.py
+++ gamu.py
@@ -1,59 +1,78 @@
 from selenium import webdriver
+from selenium.common import exceptions
 import argparse
 import time
-from selenium.webdriver.common.keys import Keys
+import keyboard
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
+class Gamu():
+    def __init__(self):
+    # defining parser object which takes arguments from command line
+        parser = argparse.ArgumentParser(usage='python gamu.py [target] [username] [password]', add_help=True)
+        # adding arguments using the parser object
+        parser.add_argument('target', help='target account')
+        parser.add_argument('username', help='username of your account')
+        parser.add_argument('password', help='password of your account')
+        # setting arguments from parser object into list variable
+        args = parser.parse_args()
+        # setting arguments into variables. RIP to RAM
+        self.target = args.target
+        self.username = args.username
+        self.password = args.password
+        # creating the mozilla driver object
+        self.serv = Service(r'C:\Users\Lords\dexterslab\lab\geckodriver.exe')
+        self.driver = webdriver.Firefox(service=self.serv)
+    
 
-# defining parser object which takes arguments from command line
-parser = argparse.ArgumentParser(usage='python gamu.py [target] [username] [password]', add_help=True)
-# adding arguments using the parser object
-parser.add_argument('target', help='target account')
-parser.add_argument('username', help='username of your account')
-parser.add_argument('password', help='password of your account')
-# setting arguments from parser object into list variable
-args = parser.parse_args()
-# setting arguments into variables. RIP to RAM
-target = args.target
-username = args.username
-password = args.password
+    
+        def reload(self):
+                driver = self.driver
+                driver.back()
+                driver.forward()
 
-# creating the mozilla driver object
-serv = Service(r'C:\Users\Lords\dexterslab\lab\geckodriver.exe')
-driver = webdriver.Firefox(service=serv)
-def reload():
-        driver.back()
-        driver.forward()
+    def login(self):
+        driver = self.driver
+        driver.get('https://instagram.com')
+        try:
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
+                (By.NAME, 'username'))).send_keys(self.username)
+            driver.find_element(By.NAME, 'password').send_keys(self.password)
+            driver.find_element(By.TAG_NAME, 'form').submit()
+            time.sleep(2)
+        except TimeoutError:
+            print('Timeout error occured')
+            
+        return driver
 
-def login(username, password):
-    driver.get('https://instagram.com')
-    WebDriverWait(driver, 10).until(EC.element_to_be_clickable(
-        (By.NAME, 'username'))).send_keys(username)
-
-    driver.find_element(By.NAME, 'password').send_keys(password)
-    driver.find_element(By.TAG_NAME, 'form').submit()
-    time.sleep(2)
-    return driver
-
-def search(target):
-
-    #driver.find_element(
-        #By.XPATH, '/html/body/div[1]/section/main/div/div/div/div/button').click()
-    # clearing notifiation thingy and searching
-    WebDriverWait(driver, 25).until(EC.element_to_be_clickable(
-        (By.XPATH, '//button[text()="Not Now"]'))).click()
-    time.sleep(2)
-    driver.find_element(By.XPATH, '//button[text()="Not Now"]').click()
-    time.sleep(3)
-    driver.find_element(By.CSS_SELECTOR, "[aria-label='Search input']").send_keys(target)
-    time.sleep(3)
-    driver.find_element(By.XPATH, '//div[text()="{}"]'.format(target)).click()
-    time.sleep(2)
-    return driver
+    def search(self):
+        driver = self.driver
+        target = self.target
+        #driver.find_element(
+            #By.XPATH, '/html/body/div[1]/section/main/div/div/div/div/button').click()
+        # clearing notifiation thingy and searching
+        WebDriverWait(driver, 25).until(EC.element_to_be_clickable(
+            (By.XPATH, '//button[text()="Not Now"]'))).click()
+        time.sleep(2)
+        driver.find_element(By.XPATH, '//button[text()="Not Now"]').click()
+        time.sleep(3)
+        try:
+            driver.find_element(By.XPATH, "/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]/div/div/div/div/div[2]/div[2]/div/a/div/div[2]/div/div").click()
+        except exceptions.NoSuchElementException:
+            driver.find_element(By.CLASS_NAME, 'coreSpriteSearchIcon').click()
+        time.sleep(3)
+        keyboard.write(target)
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[text()="{}"]'.format(target))))
+        driver.find_element(By.XPATH, '//div[text()="{}"]'.format(target)).click()
+        time.sleep(2)
+        return driver
+    
+    
+    
+    
 
 if __name__ == '__main__':
     logo = r'''
@@ -71,5 +90,7 @@ if __name__ == '__main__':
     intro = 'This is an Instagram bot built by Engineer Chiwara (@the.ip.boy.friend on Instagram). It goes to a targets recent post, goes through the people who liked it and follows them in hopes of them following back.'
     print(logo)
     print(intro)
-    login(username, password)
-    search(target)
+    
+    gamu = Gamu()
+    gamu.login()
+    gamu.search()
